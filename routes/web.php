3747247f4ecdf0bd2c5ef60cc72b2b59a93d3b13
erdemsincer <erdemsincer123@icloud.com>
2024-03-31 02:05:17 +0300

    master blade yapısı olusturuldu ve test route olusturuldu

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,17 @@ Route::middleware([
         return 5;
     });
 });
+
+
+////TEST ROUTELARI BAŞLANGIÇ
+///
+///
+Route::get('masterTest',function (){
+    return view('front.layouts.master');
+});
+///
+///
+///
+////TEST ROUTLARI BİTİŞ
+
+
